3811364a96f6de2c861828e5dfbf76a01310a5f3
HB0P <harrymilne2007@gmail.com>
2024-11-21 19:21:22 +0000

    initial

---
+++ src/client/java/dev/hbop/balancedtransport/client/BalancedTransportClient.java
@@ -0,0 +1,10 @@
+package dev.hbop.balancedtransport.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BalancedTransportClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/dev/hbop/balancedtransport/BalancedTransport.java
@@ -0,0 +1,10 @@
+package dev.hbop.balancedtransport;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BalancedTransport implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
